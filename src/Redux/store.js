267a4826db3cd2b81fb4bddbf4f85bded9fa715b
kevin zhuang <kevin7441@gmail.com>
2019-12-16 17:44:23 +1100

    added logic to hide redux dev tool in prod

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,20 @@
 // Redux Libaraies
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 // Reducers
 import MainReducer from './reducer.main';
+const sagaMiddleware = createSagaMiddleware();
 
 // using advanced enhancer to solve redux extension break problem
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
 
 // Create store
-const Store = createStore(MainReducer);
+const Store = createStore(MainReducer, enhancer);
 
 export default Store;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
