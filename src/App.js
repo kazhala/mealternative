a60134d4567146e7f39f3a65d974193c5fdcfe2e
kevin zhuang <kevin7441@gmail.com>
2019-12-16 17:05:47 +1100

    set up router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/search' component={Search} />
+        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </div>
+  );
 }
+
+const Home = () => {
+  return <div>home</div>;
+};
+
+const Search = () => {
+  return <div>search</div>;
+};
+
+const Signup = () => {
+  return <div>signup</div>;
+};
 
 export default App;
