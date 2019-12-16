4515554c629c0b8fe343cc07da9b748e268b7a29
kevin zhuang <kevin7441@gmail.com>
2019-12-16 17:57:13 +1100

    set up theme to overwrite default material ui theme

---
+++ src/Theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core';
+import { blue } from '@material-ui/core/colors';
+
+const Theme = createMuiTheme({
+  palette: {
+    primary: blue
+  }
+});
+
+export default Theme;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
-import App from './App';
+import App from './App/App';
 import './index.css';
+import { ThemeProvider } from '@material-ui/core';
+import Theme from './Theme';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={Theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
