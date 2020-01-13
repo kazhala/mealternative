9d75014f0c304419d78c9c28c26d5585f5e81b8c
kevin zhuang <kevin7441@gmail.com>
2020-01-13 11:13:44 +1100

    updated header

--- src/App/Header.js
+++ src/App/Header.js
@@ -1,55 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { withRouter } from 'react-router-dom';
-import { AppBar, Toolbar, IconButton, Button } from '@material-ui/core';
-import { Menu } from '@material-ui/icons';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Button,
+  Menu,
+  MenuItem
+} from '@material-ui/core';
+import {
+  MenuRounded,
+  HomeRounded,
+  MapRounded,
+  AccountCircle
+} from '@material-ui/icons';
 
 const Header = props => {
   const { classes, isAuthenticated, history, signOut } = props;
 
-  const handleRouteChange = name => {
-    history.push(`/${name}`);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleRouteChange = path => {
+    history.push(path);
+  };
+
+  const handleClick = e => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleMenuClick = path => {
+    handleClose();
+    handleRouteChange(path);
+  };
+
+  const handleSignOut = () => {
+    handleClose();
+    signOut();
   };
 
   return (
     <AppBar position='fixed'>
       <Toolbar disableGutters className={classes.menuBarLayout}>
-        <IconButton>
-          <Menu style={{ color: '#eceff4' }} />
-        </IconButton>
-        {isAuthenticated ? (
-          <div>
-            <Button style={{ color: '#eceff4' }} onClick={signOut}>
-              Sign Out
-            </Button>
-          </div>
-        ) : (
-          <div>
-            <Button
-              onClick={() => handleRouteChange('map')}
-              style={{ color: '#eceff4' }}
-            >
-              Map
-            </Button>
-            <Button
-              onClick={() => handleRouteChange('signin')}
-              style={{ color: '#eceff4' }}
-            >
-              Sign In
-            </Button>
-            <Button
-              onClick={() => handleRouteChange('signup')}
-              style={{ color: '#eceff4' }}
-            >
-              Sign Up
-            </Button>
-          </div>
-        )}
+        <div>
+          <IconButton style={{ color: '#eceff4' }}>
+            <MenuRounded />
+          </IconButton>
+
+          <IconButton
+            style={{ color: '#eceff4' }}
+            onClick={() => handleRouteChange('/')}
+          >
+            <HomeRounded />
+          </IconButton>
+        </div>
+
+        <div>
+          <IconButton
+            onClick={() => handleRouteChange('/map')}
+            style={{ color: '#eceff4' }}
+          >
+            <MapRounded />
+          </IconButton>
+
+          <IconButton onClick={handleClick} style={{ color: '#eceff4' }}>
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            {isAuthenticated
+              ? authMenus.map((item, index) => (
+                  <MenuItem
+                    onClick={
+                      item.path
+                        ? () => handleMenuClick(item.path)
+                        : handleSignOut
+                    }
+                    key={index}
+                  >
+                    {item.text}
+                  </MenuItem>
+                ))
+              : noAuthMenus.map((item, index) => (
+                  <MenuItem
+                    key={index}
+                    onClick={() => handleMenuClick(item.path)}
+                  >
+                    {item.text}
+                  </MenuItem>
+                ))}
+          </Menu>
+        </div>
       </Toolbar>
     </AppBar>
   );
 };
+
+const authMenus = [{ text: 'Sign Out' }];
+
+const noAuthMenus = [
+  { path: '/signin', text: 'Sign In' },
+  { path: '/signup', text: 'Sign Up' }
+];
 
 Header.propTypes = {
   classes: PropTypes.object.isRequired,
