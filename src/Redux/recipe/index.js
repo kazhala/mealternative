74e9c037da77617b18f4cc4f05bca4368a52add5
kevin zhuang <kevin7441@gmail.com>
2020-01-28 09:56:05 +1100

    inital set up for recipe redux

---
+++ src/Redux/recipe/index.js
@@ -0,0 +1,8 @@
+import RecipeReducer from './reducer';
+import * as RecipeTypes from './types';
+import * as RecipeActions from './actions';
+import * as RecipeSagas from './sagas';
+
+export { RecipeSagas, RecipeActions, RecipeTypes };
+
+export default RecipeReducer;
---
+++ src/Redux/recipe/types.js
@@ -0,0 +1,9 @@
+/*
+  Recipe related types
+*/
+
+export const RECIPE_BEGIN = 'RECIPE_BEGIN';
+export const RECIPE_ERROR = 'RECIPE_ERROR';
+export const RECIPE_SUCCESS = 'RECIPE_SUCCESS';
+
+export const RECIPE_LIKE = 'RECIPE_LIKE';
