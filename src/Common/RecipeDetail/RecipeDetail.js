74127f0e84899fe25b6d18166008fb4fdf557b03
kevin zhuang <kevin7441@gmail.com>
2020-01-31 14:39:21 +1100

    set up individual modal

--- src/Common/RecipeDetail/RecipeDetail.js
+++ src/Common/RecipeDetail/RecipeDetail.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Paper, Slide } from '@material-ui/core';
+import useStyles from './Style';
 
 const RecipeDetail = props => {
   // const { recipeDetails } = props;
+  const classes = useStyles();
 
   return (
-    <Slide direction='left' in={true} mountOnEnter unmountOnExit>
-      <Paper>hello</Paper>
+    <Slide timeout={1000} direction='left' in={true} mountOnEnter unmountOnExit>
+      <Paper className={classes.detailRecipeRoot}>hello</Paper>
     </Slide>
   );
 };
---
+++ src/Common/RecipeDetail/Style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  detailRecipeRoot: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  }
+}));
+
+export default useStyles;
