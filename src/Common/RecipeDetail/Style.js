b6588144c191a15df4a7ec958838a7009ad3ecb1
kevin zhuang <kevin7441@gmail.com>
2020-02-17 10:12:27 +1100

    fixed scrolling the overlayed content

--- src/Common/RecipeDetail/Style.js
+++ src/Common/RecipeDetail/Style.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   detailRecipeRoot: {
-    position: 'absolute',
+    position: 'fixed',
     top: 0,
     bottom: 0,
     left: 0,
