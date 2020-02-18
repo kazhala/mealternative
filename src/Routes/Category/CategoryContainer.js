560425bd394e079eedcbe5d7a61908289147eb7d
kevin zhuang <kevin7441@gmail.com>
2020-02-19 09:41:02 +1100

    get recipes from backend

--- src/Routes/Category/CategoryContainer.js
+++ src/Routes/Category/CategoryContainer.js
@@ -9,21 +9,41 @@ import React, { useEffect } from 'react';
 // redux
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { CategoryActions } from '../../Redux/category';
 
 // misc
 import queryString from 'query-string';
 
 const CategoryContainer = props => {
-  const { location, history } = props;
+  const { location, history, getCategoryRecipes } = props;
 
   useEffect(() => {
     const queryParams = queryString.parse(location.search);
     if (!queryParams.id) {
       history.replace('/');
+    } else {
+      getCategoryRecipes(queryParams.id);
     }
-  }, [location, history]);
+  }, [location, history, getCategoryRecipes]);
 
   return <div>CategoryContainer</div>;
 };
 
-export default CategoryContainer;
+const mapStateToProps = state => {
+  return {
+    loading: state.Category.loading,
+    error: state.Category.error,
+    recipes: state.Category.recipes
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      getCategoryRecipes: CategoryActions.getCategoryRecipes
+    },
+    dispatch
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryContainer);
