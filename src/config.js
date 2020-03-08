071f3b26b75c383d143c18ba2c54708a78edc1a5
kevin zhuang <kevin7441@gmail.com>
2020-03-09 08:22:31 +1100

    update env

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@
   Environment Variable export
 */
 export const GoogleMapAPIKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
-export const API = process.env.REACT_APP_BACKEND_URL;
+export const API =
+  process.env.NODE_ENV !== 'production'
+    ? process.env.REACT_APP_BACKEND_URL
+    : 'https://api.mealternative.com';
 export const CloudinaryURL = process.env.REACT_APP_CLOUDINARY_URL;
