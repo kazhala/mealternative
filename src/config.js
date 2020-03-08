7d120d00d7288c6b454531c3a40a31425202a637
kevin zhuang <kevin7441@gmail.com>
2020-03-09 09:37:12 +1100

    fix prod url

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ export const GoogleMapAPIKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
 export const API =
   process.env.NODE_ENV !== 'production'
     ? process.env.REACT_APP_BACKEND_URL
-    : 'https://api.mealternative.com';
+    : 'https://api.mealternative.com/api';
 export const CloudinaryURL = process.env.REACT_APP_CLOUDINARY_URL;
