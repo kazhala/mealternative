65401ead7c539dbab54c15c1c312a5d3c04cc5a5
kevin zhuang <kevin7441@gmail.com>
2020-04-19 10:07:55 +1000

    limit max width of recipe page

--- src/Routes/Recipes/Style.js
+++ src/Routes/Recipes/Style.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   // root
   recipeRoot: {
     width: '100%',
@@ -11,25 +11,26 @@ const useStyles = makeStyles(theme => ({
     alignItems: 'center',
     overflowY: 'auto',
     overflowX: 'hidden',
-    position: 'relative'
+    position: 'relative',
   },
   recipeSearchRoot: {
     width: '100%',
+    maxWidth: '1200px',
     display: 'grid',
     alignItems: 'center',
     gridTemplateColumns: '8fr 3fr',
     marginBottom: theme.spacing(1),
     gridGap: theme.spacing(0.5),
     [theme.breakpoints.up('sm')]: {
-      width: '75%'
+      width: '75%',
     },
     [theme.breakpoints.up('md')]: {
-      width: '65%'
-    }
+      width: '65%',
+    },
   },
   recipeSearchField: {
     width: '100%',
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
 
   // body
@@ -38,32 +39,33 @@ const useStyles = makeStyles(theme => ({
     display: 'grid',
     gridTemplateColumns: '1fr 1fr',
     gridGap: theme.spacing(1),
+    maxWidth: '1200px',
     [theme.breakpoints.up('sm')]: {
-      width: '75%'
+      width: '75%',
     },
     [theme.breakpoints.up('md')]: {
       width: '65%',
-      gridTemplateColumns: 'repeat(auto-fill, minmax(192px, 1fr))'
+      gridTemplateColumns: 'repeat(auto-fill, minmax(192px, 1fr))',
     },
     [theme.breakpoints.up('lg')]: {
-      gridTemplateColumns: 'repeat(auto-fill, minman(224px, 1fr))'
+      gridTemplateColumns: 'repeat(auto-fill, minman(224px, 1fr))',
     },
     [theme.breakpoints.up('xl')]: {
-      gridTemplateColumns: 'repeat(auto-fill, minman(256px, 1fr))'
-    }
+      gridTemplateColumns: 'repeat(auto-fill, minman(256px, 1fr))',
+    },
   },
   recipeBodyColumn: {
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 
   // speedial
   recipeDial: {
     position: 'fixed',
     right: theme.spacing(2),
-    bottom: theme.spacing(2)
-  }
+    bottom: theme.spacing(2),
+  },
 }));
 
 export default useStyles;
